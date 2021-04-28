f861eedca099acd391206e3951fb27ed3df81707
courtney-mcquade <courtney.mcquade1@marist.edu>
2021-04-27 20:11:00 -0400

    Week 11

---
+++ Final Project/hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/hello/')
+
+
+@app.route('/')
+def index():
+    return 'Courtneys Website'
+    
+
+@app.route('/aboutme/')
+def aboutme():
+    return render_template('aboutme.html')
+
