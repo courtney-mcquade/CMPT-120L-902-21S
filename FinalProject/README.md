17d2f13b7f5666998b8f39aae56677ab405da7c5
courtney-mcquade <courtney.mcquade1@marist.edu>
2021-05-24 13:39:12 -0400

    Final Project

---
+++ FinalProject/README.md
@@ -0,0 +1,25 @@
+Courtney McQuade's Website 
+
+Description 
+    For my final project in Introduction to Programming (CMPT 120-902) I created a web page all about myself. The web page uses flask, html and python to run a web page on a local host. This project manages multiple web pages and hosts static content on the webpage using basic HTML and Flask Web Server. On the website, you can find a description about me, my resume, some of my favorite music, and contact information. 
+
+Getting Started and Executing the Program 
+
+    How to Run the Program 
+    1. Download the required files 
+    2. Open bash (or your computer's terminal) within the FinalProject folder 
+    3. Run the following commands 
+    
+        export FLASK_DEBUG=1
+        export FLASK_APP=hello.py
+        flask run 
+
+    4. Open up localhost:5000 in the browser of your choice 
+    5. You can view all pages from the main page 
+    6. All the logging can be seen from the terminal window
+
+
+Author 
+    Courtney McQuade 
+
+
--- FinalProject/hello.py
+++ FinalProject/hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
+from flask import logging
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/hello/')
@@ -8,17 +9,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_templates('index.html')
+    app.logger.info("Moving to Index Page");
+    return render_template('index.html')
     
 
-@app.route('/aboutme/')
+@app.route('/aboutme')
 def aboutme():
+    app.logger.info("Moving to the About Me Page");
     return render_template('aboutme.html')
 
-@app.route('/resume/')
+
+@app.route('/resume')
 def resume():
+    app.logger.info("Moving to the Resume Page");
     return render_template('resume.html')
+
+@app.route('/music')
+def music():
+    app.logger.info("Moving to the Favorite Music Page");
+    return render_template('music.html')
+
+@app.route('/contact')
+def contact():
+    app.logger.info("Moving to the Contact Page");
+    return render_template('contact.html')
+
 
 @app.errorhandler(404)
 def page_not_found(error):
+    app.logger.info("Moving to the 404 Page");
     return render_template('404.html'), 404
